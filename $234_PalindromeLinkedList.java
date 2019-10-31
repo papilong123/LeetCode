e2c99a2ad018818b50dd0c4ab9b16aed331eccfe
running man <48591593+papilong123@users.noreply.github.com>
2019-10-31 10:39:10 +0800

    191031Update

---
+++ $234_PalindromeLinkedList.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        
+        ListNode L=new ListNode(0);
+        L.next=head;
+        
+        int len=0;
+        int i;
+        while(head!=null){
+            len++;
+            head=head.next;
+        }
+        head=L.next;
+        int[] A =new int[len/2+1];
+        
+        //i的值从1开始到len/2+1，防止下标变为-1
+        for(i=1;i<len/2+1;i++){
+            A[i]=head.val;
+            head=head.next;
+        }
+        
+        //i变为最后一个数值，如果是奇数移到下一个节点
+        i--;
+        if(len%2==1)
+            head=head.next;
+        while(head!=null&&A[i]==head.val){
+            i--;
+            head=head.next;
+        }
+        
+        
+        if(head!=null){
+            return false;
+        }
+        else
+            return true;
+    }
+}
+
+
+
+
+
+
+
