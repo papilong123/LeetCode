ea00f9a82b29ee0243a53c1f36ca850f6e3e7741
running man <48591593+papilong123@users.noreply.github.com>
2019-10-31 12:38:08 +0800

    191031Update

---
+++ java/linkedlist/$25_ReverseNodesInKGroup.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        int len=0,cycles=0;
+        ListNode p=head,r=null;
+        ListNode L =new ListNode(0) , pre=L;
+        L.next=head;
+        
+        while(p!=null){
+            len++;
+            p=p.next;
+        }
+        p=head;
+        cycles=len/k;
+        pre.next=null;
+        
+        while(cycles-- >0){
+            for(int i=0 ; i<k ; i++){
+                r=p.next;
+                p.next=pre.next;
+                pre.next=p;
+                p=r;
+            }
+            pre=head;
+            head=p;
+        }
+        //接上最后一部分
+        pre.next=p;
+        
+        return L.next;
+    }
+}
