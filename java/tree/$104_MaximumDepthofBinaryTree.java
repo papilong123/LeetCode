f1e8e1e2351c06ec67e668bf2e8d6411381fb2c2
running man <48591593+papilong123@users.noreply.github.com>
2019-11-01 13:14:32 +0800

    191101Update

--- java/tree/$104_MaximumDepthofBinaryTree.java
+++ java/tree/$104_MaximumDepthofBinaryTree.java
@@ -13,3 +13,31 @@ class Solution {
         else return java.lang.Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
+
+
+
+
+
+
+
+//good idea recursion
+class Solution {
+    int max = 0;
+    int depth = 0;
+    
+    public int maxDepth(TreeNode root) {
+        backOrder(root);
+        
+        return max;
+    }
+    
+    private void backOrder(TreeNode root) {
+        if (root != null) {
+            depth++;
+            max = Math.max(max, depth);
+            backOrder(root.left);
+            backOrder(root.right);
+            depth--;
+        }
+    }
+}
