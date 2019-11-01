9eb1f34937ea8210728417d8668fdc28f9b878e3
running man <48591593+papilong123@users.noreply.github.com>
2019-11-01 08:45:37 +0800

    191101Update

---
+++ java/linkedlist/$147_InsertionSortList.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode L =new ListNode (0);
+        ListNode pre=L,q=null;
+        L.next=null;
+        while(head!=null){
+        pre=L;
+        while(pre.next!=null&&pre.next.val<head.val){
+            pre=pre.next;
+        }
+        q=head.next;
+        head.next=pre.next;
+        pre.next=head;
+        head=q;
+        }
+        return L.next;
+    }
+}
