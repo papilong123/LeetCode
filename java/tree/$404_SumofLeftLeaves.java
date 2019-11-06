3f42938e54cb4ad654eac228d9ec7da813a9c73e
running man <48591593+papilong123@users.noreply.github.com>
2019-11-06 12:10:18 +0800

    191106Update

---
+++ java/tree/$404_SumofLeftLeaves.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root==null) return 0;
+
+        int sum=0;
+        if(isleaf(root.left)){
+            sum+=root.left.val;
+        }
+        else{
+            sum+=sumOfLeftLeaves(root.left);
+        }
+        if(root.right!=null)
+            sum+=sumOfLeftLeaves(root.right);
+        return sum;
+    }
+    public boolean isleaf(TreeNode t){
+        return (t!=null&&t.left==null&&t.right==null);
+    }
+}
