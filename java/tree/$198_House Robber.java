86393585bac6879d3e8088846c1a4c4731b63d40
running man <48591593+papilong123@users.noreply.github.com>
2019-11-09 19:07:12 +0800

    Dynamic Programming

---
+++ java/tree/$198_House Robber.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int rob(int[] nums) {
+        int pre=0,p=0;
+        for(int v: nums ){
+            int tmp=p;
+            p=Math.max(pre+v,p);
+            pre=tmp;
+        }
+        return p;
+    }
+}
