a19210ce49721e3da79e116093716cdd66d2f916
running man <48591593+papilong123@users.noreply.github.com>
2019-11-10 19:42:05 +0800

    191110Update

---
+++ java/tree/$129_Sum Root to Leaf Numbers.java
@@ -0,0 +1,21 @@
+class Solution {
+    int sum=0;
+
+    public int sumNumbers(TreeNode root) {
+        if(root==null) return 0;
+        dfs(root,root.val);
+        return sum;
+    }
+    public void dfs(TreeNode root,int k){
+        if(root.left==null&&root.right==null){
+            sum+=k;
+            return;
+        }
+        if(root.left!=null){
+            dfs(root.left,k*10+root.left.val);
+        }
+        if(root.right!=null){
+            dfs(root.right,k*10+root.right.val);
+        }
+    }
+}
