c75b26afd77c89b4253d4798e6d044833df5d7e4
running man <48591593+papilong123@users.noreply.github.com>
2019-12-23 20:01:58 +0800

    191223Update

---
+++ java/Stack/$84_LargestRectangleinHistogram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack=new Stack <> ();
+        int res=0;
+        stack.push(-1);
+        for(int i=0;i<heights.length;i++){
+            while(stack.peek()!=-1 && heights[stack.peek()]>=heights[i])
+                res=Math.max(res , heights[stack.pop()] * (i-stack.peek()-1));
+            stack.push(i);
+        }
+
+        while(stack.peek()!=-1)
+            res=Math.max(res , heights[stack.pop()] * (heights.length-stack.peek()-1));
+
+        return res;
+    }
+}
