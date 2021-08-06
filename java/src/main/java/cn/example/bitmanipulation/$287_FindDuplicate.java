5e91f2f19a2e74666a87a31554799d22bb123bd7
Run <48591593+papilong123@users.noreply.github.com>
2021-08-06 20:56:27 +0800

    2021-08-6 $287_FindDuplicate.java

--- java/src/main/java/cn/example/bitmanipulation/$287_FindDuplicate.java
+++ java/src/main/java/cn/example/bitmanipulation/$287_FindDuplicate.java
@@ -2,8 +2,11 @@ package cn.example.bitmanipulation;
 
 public class $287_FindDuplicate {
     public int findDuplicate(int[] nums) {
-        int res = 0;
+        return nums.length;
+    }
 
-        return res;
+    public static void main(String[] args) {
+        int ans = new $287_FindDuplicate().findDuplicate(new int[]{1,3,4,2,2});
+        System.out.println(ans);
     }
 }
