3d77f435979268d595b3f24a47938feed5e00e6a
Run <48591593+papilong123@users.noreply.github.com>
2021-08-07 12:08:55 +0800

    2021-08-7 $81_SearchInRotatedSortedArrayII.java

---
+++ java/src/main/java/cn/example/binarysearch/$81_SearchInRotatedSortedArrayII.java
@@ -0,0 +1,39 @@
+package cn.example.binarysearch;
+
+public class $81_SearchInRotatedSortedArrayII {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return nums[0] == target;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+//            当前二分区间的左边界加一，右边界减一，然后在新区间上继续二分查找
+            if (nums[l] == nums[mid] && nums[mid] == nums[r]) {
+                ++l;
+                --r;
+            } else if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return false;
+
+    }
+}
