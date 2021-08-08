bf5e4cdd83b7ff2273ccaa5224596b69c5ed1ed5
Run <48591593+papilong123@users.noreply.github.com>
2021-08-08 18:00:51 +0800

    2021-08-08 $5841_FindTheLongestValidObstacleCourseAtEachPosition.py

---
+++ python/cache/$5841_FindTheLongestValidObstacleCourseAtEachPosition.py
@@ -0,0 +1,22 @@
+from bisect import bisect_right
+from typing import List
+
+
+class Solution:
+    def longestObstacleCourseAtEachPosition(self, obstacles: List[int]) -> List[int]:
+        d = list()
+        ans = list()
+        for ob in obstacles:
+            # 这里需要改成 >=
+            if not d or ob >= d[-1]:
+                d.append(ob)
+                ans.append(len(d))
+            else:
+                # 将 300 题解中的二分查找改为 API 调用使得代码更加直观
+                # 如果是最长严格递增子序列，这里是 bisect_left
+                # 如果是最长递增子序列，这里是 bisect_right
+                loc = bisect_right(d, ob)
+                ans.append(loc + 1)
+                d[loc] = ob
+
+        return ans
