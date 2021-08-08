5c7a126490e9dc5b5793bbfc0104da9d5601db26
Run <48591593+papilong123@users.noreply.github.com>
2021-08-08 15:14:20 +0800

    2021-08-08 $300_LongestIncreasingSubsequence.java

---
+++ java/src/main/java/cn/example/dp/$300_LongestIncreasingSubsequence.java
@@ -0,0 +1,28 @@
+package cn.example.dp;
+
+import java.util.Arrays;
+
+public class $300_LongestIncreasingSubsequence {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int ans = new $300_LongestIncreasingSubsequence().lengthOfLIS(new int[]{1,3,6,7,9,4,10,5,6});
+        System.out.println(ans);
+    }
+}
