6404a83adc71d62bbc0d7e49ae30401088d940c8
Run <48591593+papilong123@users.noreply.github.com>
2021-08-08 20:25:00 +0800

    2021-08-08 $456_132Pattern.java

--- java/src/main/java/cn/leetcode/cache/$5841_FindTheLongestValidObstacleCourseAtEachPosition.java
+++ java/src/main/java/cn/leetcode/cache/$5841_FindTheLongestValidObstacleCourseAtEachPosition.java
@@ -1,4 +1,4 @@
-package cn.leetcode.dp;
+package cn.leetcode.cache;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ java/src/main/java/cn/leetcode/stack/$456_132Pattern.java
@@ -0,0 +1,26 @@
+package cn.leetcode.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class $456_132Pattern {
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        Deque<Integer> d = new ArrayDeque<>();
+        int k = Integer.MIN_VALUE;
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] < k) return true;
+            while (!d.isEmpty() && d.peekLast() < nums[i]) {
+                // 事实上，k 的变化也具有单调性，直接使用 k = pollLast() 也是可以的
+                k = Math.max(k, d.pollLast());
+            }
+            d.addLast(nums[i]);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        boolean ans = new $456_132Pattern().find132pattern(new int[]{3,5,0,3,4});
+        System.out.println(ans);
+    }
+}
