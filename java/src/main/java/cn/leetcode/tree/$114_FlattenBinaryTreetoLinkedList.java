09013c9272e3f5a775b9bf1019f480ae49f4a092
Run <48591593+papilong123@users.noreply.github.com>
2021-08-10 21:35:08 +0800

    2021-08-10 fix

--- java/src/main/java/cn/leetcode/tree/$114_FlattenBinaryTreetoLinkedList.java
+++ java/src/main/java/cn/leetcode/tree/$114_FlattenBinaryTreetoLinkedList.java
@@ -5,18 +5,16 @@ import cn.leetcode.common.TreeNode;
 class $114_FlattenBinaryTreetoLinkedList {
     public void flatten(TreeNode root) {
         while(root!=null){
-            if(root.left==null){
-                root=root.right;
-            }else{
-                TreeNode pre=root.left;
-                while(pre.right!=null){
-                    pre=pre.right;
+            if (root.left != null) {
+                TreeNode pre = root.left;
+                while (pre.right != null) {
+                    pre = pre.right;
                 }
-                pre.right=root.right;
-                root.right=root.left;
-                root.left=null;
-                root=root.right;
+                pre.right = root.right;
+                root.right = root.left;
+                root.left = null;
             }
+            root = root.right;
         }
     }
 }
