8bd09ce1ec683017525d3bececec4109a096054b
Run <48591593+papilong123@users.noreply.github.com>
2021-08-10 19:57:41 +0800

    2021-08-10 $239_SlidingWindowMaximum.java

---
+++ java/src/main/java/cn/leetcode/slidingwindow/$239_SlidingWindowMaximum.java
@@ -0,0 +1,23 @@
+package cn.leetcode.slidingwindow;
+
+import java.util.PriorityQueue;
+
+public class $239_SlidingWindowMaximum {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[0] != o2[0] ? o2[0] - o1[0] : o2[1] - o1[1]);
+        for (int i = 0; i < k; i++) {
+            pq.offer(new int[]{nums[i], i});
+        }
+        ans[0] = pq.peek()[0];
+        for (int i = k; i < n; i++) {
+            pq.offer(new int[]{nums[i], i});
+            while (pq.peek()[1] <= i - k) {
+                pq.poll();
+            }
+            ans[i - k + 1] = pq.peek()[0];
+        }
+        return ans;
+    }
+}
