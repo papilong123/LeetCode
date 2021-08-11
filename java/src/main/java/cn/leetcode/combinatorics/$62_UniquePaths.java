33f8a544c497d24777779f078a7fa35f0fe09f84
Run <48591593+papilong123@users.noreply.github.com>
2021-08-11 10:47:15 +0800

    2021-08-11 $62_UniquePaths.java

---
+++ java/src/main/java/cn/leetcode/combinatorics/$62_UniquePaths.java
@@ -0,0 +1,12 @@
+package cn.leetcode.combinatorics;
+
+public class $62_UniquePaths {
+    public int uniquePaths(int m, int n) {
+        long ans = 1;
+        for (int x = n, y = 1; y < m; ++x, ++y) {
+            ans = ans * x / y;
+        }
+        return (int) ans;
+    }
+
+}
