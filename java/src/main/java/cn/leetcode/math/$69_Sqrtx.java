5965df010ea36e786e405310fef26a81df127b61
Run <48591593+papilong123@users.noreply.github.com>
2021-08-16 15:54:40 +0800

    2021-08-16 $69_Sqrtx.java

---
+++ java/src/main/java/cn/leetcode/math/$69_Sqrtx.java
@@ -0,0 +1,22 @@
+package cn.leetcode.math;
+
+public class $69_Sqrtx {
+    public int mySqrt(int x) {
+        int i = 1, j = x, ans = -1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if ((long) mid * mid <= x) {
+                ans = mid;
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int ans = new $69_Sqrtx().mySqrt(2147395599);
+        System.out.println(ans);
+    }
+}
