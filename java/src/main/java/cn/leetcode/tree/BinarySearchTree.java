c67858397fda0ee773923ee0e736fda32ce4a116
Run <48591593+papilong123@users.noreply.github.com>
2021-08-17 09:34:25 +0800

    2021-08-17 BinarySearchTree.java

---
+++ java/src/main/java/cn/leetcode/tree/BinarySearchTree.java
@@ -0,0 +1,81 @@
+package cn.leetcode.tree;
+
+import cn.leetcode.common.TreeNode;
+
+public class BinarySearchTree {
+    // 98. 验证是否是二叉搜索树
+    public boolean isValidBST(TreeNode root) {
+        return isValidBSTHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBSTHelper(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+        return isValidBSTHelper(node.left, lower, node.val) && isValidBSTHelper(node.right, node.val, upper);
+    }
+
+    // 700. 二叉搜索树中的搜索,搜索指定值的节点
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || val == root.val) return root;
+
+        return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
+
+    }
+
+    // 701. 二叉搜索树中的插入操作
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        // 找到空位置插入新节点
+        if (root == null) return new TreeNode(val);
+        // if (root.val == val)
+        //     BST 中一般不会插入已存在元素
+        if (root.val < val)
+            root.right = insertIntoBST(root.right, val);
+        if (root.val > val)
+            root.left = insertIntoBST(root.left, val);
+        return root;
+    }
+
+    // 450. 删除二叉搜索树中的节点
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        if (root.val == key) {
+            // 这两个 if 把情况 1 和 2 都正确处理了
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+            // 处理情况 3
+            TreeNode minNode = getMin(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, minNode.val);
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        }
+        return root;
+    }
+
+    private TreeNode getMin(TreeNode node) {
+        // BST 最左边的就是最小的
+        while (node.left != null) node = node.left;
+        return node;
+    }
+
+    // 96. 不同的二叉搜索树,恰由 n 个节点组成且节点值从 1 到 n 互不相同的二叉搜索树的种类数，恰好是卡特兰数,动态规划解
+    // https://leetcode-cn.com/problems/unique-binary-search-trees/solution/hua-jie-suan-fa-96-bu-tong-de-er-cha-sou-suo-shu-b/
+    public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+}
