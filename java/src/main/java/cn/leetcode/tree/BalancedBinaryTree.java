12cd5c7b82bca5603bbd13cec69c38fe82578c80
Run <48591593+papilong123@users.noreply.github.com>
2021-08-17 10:03:18 +0800

    2021-08-17 BalancedBinaryTree.java

---
+++ java/src/main/java/cn/leetcode/tree/BalancedBinaryTree.java
@@ -0,0 +1,20 @@
+package cn.leetcode.tree;
+
+import cn.leetcode.common.TreeNode;
+
+public class BalancedBinaryTree {
+    // 110. 平衡二叉树, 验证
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+
+    private int height(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
+
+}
