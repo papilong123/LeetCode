d9ea4a48e4e3e2df873e8760ddec9c3e71abc3cc
Run <48591593+papilong123@users.noreply.github.com>
2021-08-17 15:18:32 +0800

    2021-08-17 fix

---
+++ java/src/main/java/cn/leetcode/array/$80_RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,21 @@
+package cn.leetcode.array;
+
+public class $80_RemoveDuplicatesFromSortedArrayII {
+    public int removeDuplicates(int[] nums) {
+        return process(nums, 2);
+    }
+
+    int process(int[] nums, int k) {
+        int u = 0;
+        for (int x : nums) {
+            if (u < k || nums[u - k] != x) nums[u++] = x;
+        }
+        return u;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3};
+        int ans = new $80_RemoveDuplicatesFromSortedArrayII().removeDuplicates(nums);
+        System.out.println(ans);
+    }
+}
