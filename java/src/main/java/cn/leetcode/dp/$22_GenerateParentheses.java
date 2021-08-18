643b8cac9f6420e44b35b7c0272909cdb6fba382
Run <48591593+papilong123@users.noreply.github.com>
2021-08-18 08:58:42 +0800

    2021-08-18 $22_GenerateParentheses.java

---
+++ java/src/main/java/cn/leetcode/dp/$22_GenerateParentheses.java
@@ -0,0 +1,29 @@
+package cn.leetcode.dp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class $22_GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+        List<List<String>> dp = new ArrayList<>();
+
+        dp.add(new ArrayList<String>() {{
+            add("");
+        }});
+        for (int i = 1; i <= n; i++) {
+
+            List<String> t = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+                List<String> str1 = dp.get(j);
+                List<String> str2 = dp.get(i - 1 - j);
+                for (String s1 : str1) {
+                    for (String s2 : str2) {
+                        t.add("(" + s1 + ")" + s2);
+                    }
+                }
+            }
+            dp.add(t);
+        }
+        return dp.get(n);
+    }
+}
